232940c78584b85e5452b9e7426439af286ab3e9
wdjkwd <learnle@163.com>
2018-12-30 19:02:06 +0800

    初始化2

---
+++ springboot-filter/src/main/java/com/huo/SpringbootFilterApplication.java
@@ -0,0 +1,14 @@
+package com.huo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootFilterApplication.class, args);
+	}
+
+}
+
